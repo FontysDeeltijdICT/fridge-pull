4abe619089341200d3dda1d71c40d322e39c9c99
Wickex <wickex@users.noreply.github.com>
2021-06-09 13:55:33 +0200

    Add initial InfluxDB query builder

---
+++ InfluxDb/FluxQueryBuilder.cs
@@ -0,0 +1,50 @@
+﻿namespace FridgePull.InfluxDb
+{
+    public class FluxQueryBuilder
+    {
+        private string _fluxQuery;
+        
+        public FluxQueryBuilder(string bucket)
+        {
+            AddBucket(bucket);
+        }
+
+        public string GetResult()
+        {
+            return _fluxQuery;
+        }
+
+        public FluxQueryBuilder AddFilter(string key, string value)
+        {
+            _fluxQuery += $"|> filter(fn: (r) => r[\"{key}\"] == \"{value}\")";
+            
+            return this;
+        }
+
+        public FluxQueryBuilder AddRange(string start, string stop)
+        {
+            _fluxQuery += $"|> range(start: {start}, stop: {stop})";
+            
+            return this;
+        }
+        
+        public FluxQueryBuilder AddRange(string start)
+        {
+            _fluxQuery += $"|> range(start: {start})";
+            
+            return this;
+        }
+
+        public FluxQueryBuilder Last()
+        {
+            _fluxQuery += "|> last()";
+
+            return this;
+        }
+
+        private void AddBucket(string bucket)
+        {
+            _fluxQuery += $"from(bucket: \"{bucket}\")";
+        }
+    }
+}
