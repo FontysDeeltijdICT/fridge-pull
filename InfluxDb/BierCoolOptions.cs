c8c3b8aef461d595477e30e3e57e222140c16ad7
Wickex <wickex@users.noreply.github.com>
2021-06-09 14:21:42 +0200

    Add initial Bier.cool options

---
+++ InfluxDb/BierCoolOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace FridgePull.InfluxDb
+{
+    public class BierCoolOptions
+    {
+        public const string BierCool = "BierCool";
+
+        public string Measurement { get; set; }
+        public string Version { get; set; }
+    }
+}
