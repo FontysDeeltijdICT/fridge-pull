e014e50e1c852b5eba85c197e065bd9f70af1ca8
Wickex <wickex@users.noreply.github.com>
2021-06-09 14:22:19 +0200

    Add initial MeasurementService

---
+++ InfluxDb/MeasurementService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using FridgePull.InfluxDb.Models;
+
+namespace FridgePull.InfluxDb
+{
+    public class MeasurementService
+    {
+        private readonly MeasurementRepository _measurementRepository;
+
+        public MeasurementService(MeasurementRepository measurementRepository)
+        {
+            _measurementRepository = measurementRepository;
+        }
+
+        public IEnumerable<Measurement> GetLastMeasurement(string macAddress)
+        {
+            var fromDateTime = DateTime.Now.AddMonths(-1);
+
+            return GetLastMeasurement(macAddress, fromDateTime);
+        }
+
+        public IEnumerable<Measurement> GetLastMeasurement(string macAddress, DateTime fromDateTime)
+        {
+            return _measurementRepository.GetLastMeasurement(macAddress, fromDateTime);
+        }
+    }
+}
