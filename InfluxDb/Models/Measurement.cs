714d1981fdbb003b6bc1fae089cf2553695d6fad
Wickex <wickex@users.noreply.github.com>
2021-06-09 20:47:15 +0200

    Add more data to Measurement model

--- InfluxDb/Models/Measurement.cs
+++ InfluxDb/Models/Measurement.cs
@@ -6,16 +6,22 @@ namespace FridgePull.InfluxDb.Models
     [InfluxDB.Client.Core.Measurement("beer")]
     public class Measurement
     {
-        [Column("sensor", IsTag = true)]
-        public int Id { get; set; }
+        [Column("device", IsTag = true)]
+        public string Device { get; set; }
         
-        [Column("value")]
-        public double Temperature { get; set; }
+        [Column("sensor", IsTag = true)]
+        public int Sensor { get; set; }
         
         [Column(IsTimestamp = true)]
         public DateTime Time { get; set; }
         
-        [Column("presence")]
+        [Column("value")]
+        public double Temperature { get; set; }
+        
+        [Column("presence", IsTag = true)]
         public bool Presence { get; set; }
+        
+        [Column("version", IsTag = true)]
+        public string Version { get; set; }
     }
 }
