d65c378735f4c124694d24f75a181912e16df870
Wickex <wickex@users.noreply.github.com>
2021-06-09 10:44:36 +0200

    Add InfluxDB options class

---
+++ InfluxDb/InfluxDbOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace FridgePull.InfluxDb
+{
+    public class InfluxDbOptions
+    {
+        public const string InfluxDb = "InfluxDb";
+
+        public string Host { get; set; }
+        public string Token { get; set; }
+        public string Organization { get; set; }
+        public string Bucket { get; set; }
+    }
+}
