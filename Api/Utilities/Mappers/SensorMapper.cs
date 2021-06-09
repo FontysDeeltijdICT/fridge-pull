3121eb1fe960380d8b198ddf73b01074a313c099
Wickex <wickex@users.noreply.github.com>
2021-06-09 15:47:29 +0200

    Add model to DTO mappers

---
+++ Api/Utilities/Mappers/SensorMapper.cs
@@ -0,0 +1,15 @@
+﻿using FridgePull.Api.Dtos;
+using Measurement = FridgePull.InfluxDb.Models.Measurement;
+
+namespace FridgePull.Api.Utilities.Mappers
+{
+    public static class SensorMapper
+    {
+        public static Sensor ToDto(Measurement measurement) => new()
+        {
+            Id = measurement.Id,
+            Temperature = measurement.Temperature,
+            Presence = measurement.Presence
+        };
+    }
+}
